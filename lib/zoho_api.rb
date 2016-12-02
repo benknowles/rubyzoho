7ce9ef112337615bbcf0d947365deed54e1ae3c9
Ben Knowles <bknowles@patternmarketinginc.com>
2016-12-02 14:18:41 -0600

    Remove hidden module (causing Zoho Error Code 4918)

--- lib/zoho_api.rb
+++ lib/zoho_api.rb
@@ -27,7 +27,7 @@ module ZohoApi
 
     def initialize(auth_token, modules, ignore_fields, fields = nil)
       @auth_token = auth_token
-      @modules = %w(Accounts Contacts Events Leads Potentials Tasks Users).concat(modules).uniq
+      @modules = %w(Accounts Contacts Events Potentials Tasks Users).concat(modules).uniq
       @module_fields = fields.nil? ? reflect_module_fields : fields
       @ignore_fields = ignore_fields
     end
